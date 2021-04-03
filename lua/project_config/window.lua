710b02c12723b4a9c85f9e1ceed19046d28af9bd
Alessandro Martini <alessandro.martini@loadsmart.com>
2021-04-03 20:00:10 -0300

    style: format file

--- lua/project_config/window.lua
+++ lua/project_config/window.lua
@@ -4,8 +4,8 @@ local Path = require('plenary.path')
 local float = require('plenary.window.float')
 
 local function wincmd(win_id, cmd)
-    local winnr = vim.fn.win_id2win(win_id)
-    return ("%dwincmd %s"):format(winnr, cmd)
+  local winnr = vim.fn.win_id2win(win_id)
+  return ("%dwincmd %s"):format(winnr, cmd)
 end
 
 -- TODO(martini97, 2021-04-03): allow user to override mappings
@@ -17,20 +17,20 @@ function window.preview(file)
   }
   local win = float.centered_with_top_win(message, {winblend = 0})
 
-    local close_cmd = ":" .. wincmd(win.win_id, "c") .. "<cr>"
+  local close_cmd = ":" .. wincmd(win.win_id, "c") .. "<cr>"
 
-    vim.api.nvim_buf_set_option(win.bufnr, "filetype", "vim")
+  vim.api.nvim_buf_set_option(win.bufnr, "filetype", "vim")
 
-    vim.api.nvim_buf_set_keymap(win.bufnr, "n", "<a-q>", close_cmd,
-                                {noremap = true})
+  vim.api.nvim_buf_set_keymap(win.bufnr, "n", "<a-n>", close_cmd,
+                              {noremap = true})
 
-    vim.api.nvim_buf_set_keymap(win.bufnr, "n", "<a-y>",
-                                [[:lua require"project_config".trust_file()<cr>]],
-                                {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(win.bufnr, "n", "<a-y>",
+                              [[:lua require"project_config".trust_file()<cr>]],
+                              {noremap = true, silent = true})
 
-    vim.api.nvim_buf_set_lines(win.bufnr, 0, -1, false, file:readlines())
+  vim.api.nvim_buf_set_lines(win.bufnr, 0, -1, false, file:readlines())
 
-    vim.defer_fn(function() vim.cmd(":" .. wincmd(win.win_id, "w")) end, 20)
+  vim.defer_fn(function() vim.fn.win_gotoid(win.win_id) end, 10)
 end
 
 return window
